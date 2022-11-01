d38eee829ee4a9d5cf9530e53f5fb0d4e2e0ef7b
Slacky <98531038+Slacky300@users.noreply.github.com>
2022-11-01 17:01:03 +0530

    Update Exp10.c

--- Exp10.c
+++ Exp10.c
@@ -39,3 +39,48 @@ void main(){
     scanf("%d",&s);
     dfs(n,adj,visited,s);
 }
+
+//For Turbo
+#include <stdio.h>
+#include <conio.h>
+#define n 5
+
+void dfs(int adj[][n],int visited[],int start){
+
+    int stack[n];
+    int top = -1,i;
+    for(i = 0;i<n;i++){
+	visited[i]=0;
+    }
+    stack[++top] = start;
+    visited[start] = 1;
+
+    while(top!=-1){
+	start = stack[top--];
+	printf("%c-",start+65);
+	for(i = 0;i<n;i++){
+	    if(adj[start][i] && visited[i]==0){
+		stack[++top] = i;
+		visited[i] = 1;
+
+	    }
+	}
+    }
+}
+
+void main(){
+
+    int s,i,j;
+    int visited[n],adj[n][n];
+    clrscr();
+    printf("Enter the adjaceny matrix\n");
+    for(i = 0;i<n;i++){
+	for(j = 0;j<n;j++){
+	    scanf("%d",&adj[i][j]);
+	}
+    }
+    printf("Enter the source : ");
+    scanf("%d",&s);
+    dfs(adj,visited,s);
+    getch();
+}
