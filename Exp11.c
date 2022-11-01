f5323522241791d8593db46d3ac618347656e28b
Slacky <98531038+Slacky300@users.noreply.github.com>
2022-11-01 16:40:40 +0530

    Update Exp11.c

--- Exp11.c
+++ Exp11.c
@@ -34,3 +34,41 @@ void bfs(int y,int x,int a[],int g[][x]){
     }
 
 }
+//For Turbo C: 
+
+#include <stdio.h>
+#include <conio.h>
+#define n 5
+
+void bfs(int y,int a[],int g[][n]){
+    int k;
+    printf("%d ",y);
+    a[y] = 1;
+    for(k = 0;k<5;k++){
+	if(!a[k] && g[y][k]==1){
+	    bfs(k,a,g);
+	}
+    }
+
+}
+
+void main(){
+
+    int start,i,j;
+    int graph[n][n],visited[n];
+    clrscr();
+    printf("Enter the adjacency matrix \n");
+    for(i = 0;i<n;i++){
+	for(j = 0;j<n;j++){
+	    scanf("%d",&graph[i][j]);
+	}
+    }
+    for(i = 0;i<n;i++){
+	visited[i] = 0;
+    }
+    printf("Enter starting point ");
+    scanf("%d",&start);
+    bfs(start,visited,graph);
+    getch();
+}
+
